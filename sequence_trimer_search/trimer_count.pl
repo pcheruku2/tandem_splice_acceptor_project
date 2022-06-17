d4707b3854db6fc0c0efe3ae524fadbdec6f89dc
Cherukuri <703398698@SSXFRSCTIMGA002.sanfordhealth.org>
2022-06-17 13:09:52 -0500

    added strand information to bedfile

--- sequence_trimer_search/trimer_count.pl
+++ sequence_trimer_search/trimer_count.pl
@@ -1,20 +1,39 @@
 #!/usr/bin/perl
 
+#############################################################################################################
+# script name : trimer_count.pl
+#
+# purpose : Given a trimers of interest look up file and fasta sequences 
+# of fixed length (31 bases in exon, 33 bases in intron; with one base overlap at junction),
+# the goal of the script is to search the primary input fasta sequence for trimers of interest
+# and return a bedfile with genomic coordinates, the trimer, local coordinates relative to the junction, 
+# and strand infromation; it also returns a file with a summary of counts of trimers found. 
+#############################################################################################################
+
+
+
 use strict;
+
+#########
+# Input #
+#########
 
 my $trimer_file = $ARGV[0];
 my $fasta_file = $ARGV[1];
-my $chrom      = $ARGV[2];
+my $suffix      = $ARGV[2];
 
-my $frequency_file = "counts_$chrom.out";
-my $trimer_sites = "trimer_sites_$chrom.out";
-my $trimer_sites_bedfile = "trimer_sites_$chrom.bed";
 
+# Output file names
+my $frequency_file = "counts_$suffix.out";
+my $trimer_sites_bedfile = "trimer_sites_$suffix.bed";
+
+
+# Open output files 
 
 open C, "> $frequency_file" or die "Cannot open $frequency_file : $!";
-open T, "> $trimer_sites" or die "Cannot open $trimer_sites : $!";
 open B, "> $trimer_sites_bedfile" or die "Cannot open $trimer_sites_bedfile : $!";
 
+# Hashes & variables 
 
 my %trimer_hash =();
 my %trimer_count=();
@@ -26,6 +45,15 @@ my $cn=();
 my $from_base1=();
 my $to_base1=();
 my $seq_class=();
+my $strand=();
+
+
+#######################################################
+# Read in the trimer look up table
+# Table format: 
+# trimer and consequence of variant at 2nd base
+# AAA	G:gain 
+#######################################################
 
 open F, "< $trimer_file" or die "Cannot open $trimer_file :$!";
 while(<F>){
@@ -36,6 +64,11 @@ while(<F>){
 close F;
 
 
+#######################################################################################
+# Read in fasta sequence : 
+#    intronic sequences = 33 bases; 1 base into exon at junction
+#    exonic sequences = 31 bases; 1 base into intron at junction
+#######################################################################################
 
 open F, "< $fasta_file" or die "Cannot open $fasta_file : $!";
 while(<F>){
@@ -43,10 +76,19 @@ while(<F>){
 	$c++;
 	my $mod=$c%2;
 	if($_=~/^>/ && $mod==1){
-		$_=~/^>(.*):(\d+)-(\d+)/;		
-		$chr=$1;
-		$from_base1=$2;
-		$to_base1=$3;
+		if($_=~/rc/){ 			
+			$_=~/^>(.*):(\d+)-(\d+)\/rc/;		
+			$chr=$1;
+			$from_base1=$2;
+			$to_base1=$3;
+			$strand="-";
+		}else{ 
+			$_=~/^>(.*):(\d+)-(\d+)/;		
+			$chr=$1;
+			$from_base1=$2;
+			$to_base1=$3;
+			$strand ="+";
+		}
 		$sequence_count++;
 		
 	}elsif($mod==0){
@@ -75,8 +117,9 @@ while(<F>){
 				my $lcd=$coord+1;
 				my $cn=$chr;
 				$cn=~s/chr//g;
-				print T "$chr\t$coord\t$trimer:$local_coordinate\n";
-				print B "$cn\t$coord\t$lcd\t$trimer:$local_coordinate\n";
+
+				# Write to bedfile
+				print B "$cn\t$coord\t$lcd\t$trimer:$local_coordinate:$strand\n";
 				$trimer_count{$trimer}++;
 			}
                 }
@@ -84,10 +127,13 @@ while(<F>){
 		$chr=();
 		$from_base1=();
 		$to_base1=();
+		$strand=();
 	}		
 }
 close F;
-
+########################################################
+# Write out basic trimer count statistics to output file
+########################################################
 
 foreach my $k(sort keys %trimer_hash){
 	if($trimer_count{$k}){
@@ -97,6 +143,5 @@ foreach my $k(sort keys %trimer_hash){
 	}
 }
 
-close T;
 close C;
 close B;
